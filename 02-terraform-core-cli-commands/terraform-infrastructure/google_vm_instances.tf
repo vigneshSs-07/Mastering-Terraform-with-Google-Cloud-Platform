c5629f102ff47f15a7a97e341047afc9ac1e5c9d
vignesh sekar <vignesh1271@gmail.com>
2025-04-26 16:50:29 +0530

    terraform core cli commands

--- 02-terraform-core-cli-commands/terraform-infrastructure/google_vm_instances.tf
+++ 02-terraform-core-cli-commands/terraform-infrastructure/google_vm_instances.tf
@@ -1,69 +1,75 @@
-resource "google_compute_network" "vpc_network" {
-  name = "terraform-network"
+locals {
+  services = [
+    "compute.googleapis.com",
+    "storage.googleapis.com",
+    "serviceusage.googleapis.com",
+    "cloudresourcemanager.googleapis.com"
+  ]
 }
 
-resource "google_compute_instance" "vm_instance" {
-  name         = "terraform-instance"
+resource "google_project_service" "enabled_apis" {
+  for_each           = toset(local.services)
+  service            = each.key
+  disable_on_destroy = false
+}
+
+# Create a single Compute Engine instance 
+resource "google_compute_instance" "vm_instance_1" {
+  name         = "vm-instance-1"
   machine_type = "f1-micro"
-  tags         = ["web", "dev"]
+  tags         = ["web", "caa"] # "dev"
 
   boot_disk {
     initialize_params {
       image = "debian-cloud/debian-11"
-      #image = "cos-cloud/cos-stable"
-    }
-  }
-
-  network_interface {
-    network = google_compute_network.vpc_network.name
-    access_config {
-    }
-  }
-}
-
-
-
-
-# Create a single Compute Engine instance
-resource "google_compute_instance" "flask_app_1" {
-  name         = "flask-vm"
-  machine_type = "f1-micro"
-  zone         = "us-west1-a"
-  tags         = ["ssh"]
-
-  boot_disk {
-    initialize_params {
-      image = "debian-cloud/debian-11"
+      labels = {
+        my_label = "cloud_ai_analytics"
+      }
     }
   }
 
   # Install Flask
-  metadata_startup_script = "sudo apt-get update; sudo apt-get install -yq build-essential python3-pip rsync; pip install flask"
+  metadata_startup_script = "echo hi > /test.txt" # echo hi Cloud & AI Analytics
 
   network_interface {
     subnetwork = google_compute_subnetwork.subnet_1.id
 
     access_config {
-      # Include this section to give the VM an external IP address
     }
   }
 
   scheduling {
-    preemptible = true
-    automatic_restart = false
+    preemptible        = true
+    automatic_restart  = false
     provisioning_model = "SPOT"
   }
 }
 
-resource "google_compute_network" "main_vpc" {
-  name                    = "main-vpc"
+resource "google_compute_network" "vm_vpc_network" {
+  name                    = "vminstance-vpc"
   auto_create_subnetworks = false
   mtu                     = 1460
 }
 
+
 resource "google_compute_subnetwork" "subnet_1" {
-  name          = "subnet-1"
+  name          = "subnetid-1"
   ip_cidr_range = "10.0.1.0/24"
-  region        = "us-west1-a"
-  network       = google_compute_network.main_vpc.id
+  network       = google_compute_network.vm_vpc_network.id
+}
+
+
+
+resource "google_compute_firewall" "allow_iap_ssh" {
+  name      = "allow-iap-ssh-ingress"
+  network   = google_compute_network.vm_vpc_network.id
+  direction = "INGRESS"
+
+  allow {
+    protocol = "tcp"
+    ports    = ["22"]
+  }
+
+  source_ranges = ["35.235.240.0/20"]
+  target_tags   = ["caa"]
 }
